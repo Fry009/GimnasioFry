8e2f2bb1185d15a7e13a9ac5b99d63ae3fc0f107
FRAN\fmdia <fmdiazmontes87@gmail.com>
2024-04-30 17:35:22 +0200

    springboot-banckend arquitectura : fallos de despliegue

---
+++ gimnasio-fry-backend/src/main/java/com/app/gimnasiofrybackend/GimnasioFryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.gimnasiofrybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimnasioFryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimnasioFryBackendApplication.class, args);
+	}
+
+}
---
+++ gimnasio-fry-backend/src/test/java/com/app/gimnasiofrybackend/GimnasioFryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gimnasiofrybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimnasioFryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
