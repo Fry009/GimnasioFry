44db6ffc89c4e7c0f2014688131e6b2f25c3b6b0
FRAN\fmdia <fmdiazmontes87@gmail.com>
2024-05-08 15:46:09 +0200

    pagina de login con los imports de angular material

--- gimnasio-fry-front/src/app/components/input-imagen/input-imagen.component.ts
+++ gimnasio-fry-front/src/app/components/input-imagen/input-imagen.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Component, Input } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';  // Importa MatInputModule
 
 @Component({
   selector: 'app-input-imagen',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule,MatFormFieldModule,MatIconModule],  // Asegúrate de que MatInputModule está en la lista de imports
   templateUrl: './input-imagen.component.html',
-  styleUrl: './input-imagen.component.scss'
+  styleUrls: ['./input-imagen.component.scss']  // Corrige styleUrl a styleUrls
 })
 export class InputImagenComponent {
+  @Input() errorMessage: string = ''; // Propiedad de entrada para el mensaje de error
+  @Input() imageSrc: string = ''; // Propiedad de entrada para la ruta de la imagen
 
+  showError: boolean = false;
+
+  onInput(event: any) {
+    // Aquí puedes agregar tu lógica para validar el valor del input
+    const inputValue = event.target.value;
+    this.showError = inputValue.length < 3; // Muestra el error si la longitud del valor es menor que 3
+  }
 }
